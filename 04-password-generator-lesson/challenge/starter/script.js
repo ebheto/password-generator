d1fe21de571093f54d51beded5f6c6b9dc09a5f7
ebheto <ebhetome@protonmail.com>
2023-01-17 23:48:02 +0000

    finished code

--- 04-password-generator-lesson/challenge/starter/script.js
+++ 04-password-generator-lesson/challenge/starter/script.js
@@ -90,17 +90,96 @@ var upperCasedCharacters = [
 
 // Function to prompt user for password options
 function getPasswordOptions() {
+  //variable to store length of password from user imput
+  let length = parseInt(
+    prompt("How many caracters would you like your password to contain?")
+  )
 
+  if(isNaN(length) === true){
+    alert('Password length must be provided as number');
+    return;
+  }
+  if(length < 10) {
+    alert('Password length must be at least 10 characters');
+    return;
+  }
+  if(length > 64) {
+    alert ('Password length must be less than 65 characters');
+    return;
+  }
+  let hasSpecialCharacters = confirm(
+    "Click OK to confirm including special characters"
+  )
+  let hasNumericCharacters = confirm(
+    "Click OK to confirm including numeric characters"
+  )
+  let hasLowerCasedCharacters = confirm(
+    "Click OK to confirm including lowercase characters"
+  )
+  let hasUpperCasedCharacters = confirm(
+    "Click OK to confirm including uppercase characters"
+  )
+    if(hasUpperCasedCharacters === false &&
+      hasLowerCasedCharacters === false &&
+      hasNumericCharacters === false &&
+      hasSpecialCharacters === false ){
+        alert ('Must select at least one character type');
+        return;
+      }
+      let passwordOptions = {
+        length: length,
+        hasSpecialCharacters: hasSpecialCharacters,
+        hasUpperCasedCharacters: hasUpperCasedCharacters,
+        hasLowerCasedCharacters: hasLowerCasedCharacters,
+        hasNumericCharacters: hasNumericCharacters,
+      }
+      return passwordOptions;
 }
 
 // Function for getting a random element from an array
 function getRandom(arr) {
+let randomIndex = Math.floor(Math.random() * arr.length)
+let randomElement = arr[randomIndex];
 
+return randomElement; 
 }
 
 // Function to generate password with user input
 function generatePassword() {
+  let options = getPasswordOptions();
+  console.log(options);
+  let result = []
+  let possibleCharacter = []
+  let guaranteedCharacter = []
 
+  if(options.hasSpecialCharacters) {
+    possibleCharacter = possibleCharacter.concat(specialCharacters);
+    guaranteedCharacter.push(getRandom(specialCharacters))
+  }
+
+  if(options["hasLowerCasedCharacters"]) {
+    possibleCharacter = possibleCharacter.concat(lowerCasedCharacters);
+    guaranteedCharacter.push(getRandom(lowerCasedCharacters))
+  }
+
+  if(options.hasUpperCasedCharacters) {
+    possibleCharacter = possibleCharacter.concat(upperCasedCharacters);
+    guaranteedCharacter.push(getRandom(upperCasedCharacters))
+  }
+
+  if(options.hasNumericCharacters) {
+    possibleCharacter = possibleCharacter.concat(numericCharacters);
+    guaranteedCharacter.push(getRandom(numericCharacters))
+  }
+
+  for(let index = 0; index < options.length; index ++){
+    var generated = getRandom(possibleCharacter);
+    console.log(generated);
+    result.push(generated);
+  }
+  console.log(result);
+
+return result.join("")
 }
 
 // Get references to the #generate element
@@ -116,3 +195,15 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
+
+// Pseudocode
+// 
+// create prompts to ask user password requirements;
+// - how many characters required in password? P
+// - are special characters allowed? C
+// - are numbers allowed? C
+// - are lowercases allowed? C
+// - are uppercases allowed? C
+// 
+// code to validate user entry - to ensure correct data type is entered by customer 
+// alert to let user know wrong input entered
